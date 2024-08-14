ac43d8473f5e68b9871d6b05e95d2da0bafbd7a4
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-08-14 08:14:17 -0300

    Creating new file for weather view and adding it as destination from home view

---
+++ Weather App/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  Weather App
+//
+//  Created by Gustavo Barreto on 14/08/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
