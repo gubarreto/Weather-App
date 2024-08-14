a20a980f237e14640027244db451b4af5a1ac9ca
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-08-14 00:52:24 -0300

    Creating home view and custom tab bar

---
+++ Weather App/Views/Components/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  Weather App
+//
+//  Created by Gustavo Barreto on 14/08/24.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
