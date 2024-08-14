0180541b91ae3b8322412b198a1f6c9a6613f520
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-08-14 18:47:38 -0300

    Creating an arc shape for tab bar

---
+++ Weather App/Shapes/Arc.swift
@@ -0,0 +1,18 @@
+//
+//  Arc.swift
+//  Weather App
+//
+//  Created by Gustavo Barreto on 14/08/24.
+//
+
+import SwiftUI
+
+struct Arc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Arc()
+}
