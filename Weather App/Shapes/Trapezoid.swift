a8d02c10ca0a4dc106e7e9fc443e34f899a0fcb9
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-08-17 11:39:02 -0300

    Adding trapezoid shape and renaming weatherArray to forecasts

---
+++ Weather App/Shapes/Trapezoid.swift
@@ -0,0 +1,18 @@
+//
+//  Trapezoid.swift
+//  Weather App
+//
+//  Created by Gustavo Barreto on 17/08/24.
+//
+
+import SwiftUI
+
+struct Trapezoid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Trapezoid()
+}
