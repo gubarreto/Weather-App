3d4c1fce34dbb292ef5ff9341fc6e08f8b2b523f
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-08-17 16:04:57 -0300

    Creating forecast web service using combine and fetching by city name

--- Weather App/Services/ForecastWebService.swift
+++ Weather App/Services/ForecastWebService.swift
@@ -6,5 +6,43 @@
 //
 
 import Foundation
+import Combine
 
+enum ForecastWebServiceError: Error {
+    case invalidUrl
+    case invalidResponse
+    case decodingError
+}
+
+protocol ForecastWebServiceProtocol {
+    func fetchForecast(for city: String) -> AnyPublisher<Forecast, ForecastWebServiceError>
+}
+
+class ForecastWebService: ForecastWebServiceProtocol {
+    
+    private let baseUrl: String = "https://api.openweathermap.org/data/2.5/weather"
+    private let apiKey: String? = ProcessInfo.processInfo.environment["apiKey"]
+    
+    func fetchForecast(for city: String) -> AnyPublisher<Forecast, ForecastWebServiceError> {
+        guard var urlComponents = URLComponents(string: baseUrl) else {
+            return Fail(error: ForecastWebServiceError.invalidUrl).eraseToAnyPublisher()
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "q", value: city),
+            URLQueryItem(name: "appid", value: apiKey),
+            URLQueryItem(name: "units", value: "metric")
+        ]
+        
+        guard let url = urlComponents.url else {
+            return Fail(error: ForecastWebServiceError.invalidUrl).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: Forecast.self, decoder: JSONDecoder())
+            .mapError { _ in ForecastWebServiceError.decodingError }
+            .eraseToAnyPublisher()
+    }
+}
 
